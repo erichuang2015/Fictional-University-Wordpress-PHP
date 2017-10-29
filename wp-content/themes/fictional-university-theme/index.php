1086148e0dec3d35af7845bb1601db3e380bfe21
Rafa Huelin <it@engisa.com>
2017-10-29 05:05:40 +0000

    Testing PHP

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,11 @@
-This is our amzing custom theme.
+<?php
+    function greet($name, $color) {
+        echo "<p>Hi, my $name is blank and my favourite color is $color.</p>";
+    }
+
+    greet('John', 'blue');
+    greet('Jane', 'pink');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
