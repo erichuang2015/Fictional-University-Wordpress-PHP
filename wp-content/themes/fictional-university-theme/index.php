c330096eefc15703815484b12a39201ea7ceffd4
Rafa Huelin <it@engisa.com>
2017-10-29 05:52:50 +0000

    Use The Loop in index, posts and pages

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,12 +1,8 @@
 <?php
-    $names = array('Brad', 'John', 'Jane', 'Meowsalot');
-
-    $count = 0;
-
-    while($count < count($names)) {
-        echo "<li>Hi, my name is $names[$count]</li>";
-        $count++;
-    }
-
+	while(have_posts()) {
+		the_post(); ?>
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<?php the_content(); ?>
+		<hr>
+	<?php }
 ?>
-
