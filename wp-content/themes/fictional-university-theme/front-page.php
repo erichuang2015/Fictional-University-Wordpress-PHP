d840363212486f829d6118116b56e86fe9ea5a73
Rafa Huelin <it@engisa.com>
2017-11-01 09:26:59 +0000

    Setup event custom field using advanced custom fields

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -3,7 +3,7 @@
 <div class="page-banner">
 	<div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg'); ?>);"></div>
 	<div class="page-banner__content container t-center c-white">
-		<h1 class="headline headline--large">Welcome!!!!!</h1>
+		<h1 class="headline headline--large">Welcome!</h1>
 		<h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
 		<h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
 		<a href="#" class="btn btn--large btn--blue">Find Your Major</a>
@@ -26,8 +26,11 @@
 
                     <div class="event-summary">
                         <a class="event-summary__date t-center" href="<?php  ?>">
-                            <span class="event-summary__month"><?php the_time('M') ?></span>
-                            <span class="event-summary__day"><?php the_time('d'); ?></span>
+                            <span class="event-summary__month"><?php
+                                $eventDate = new DateTime(get_field('event_date'));
+                                echo $eventDate->format('M');
+                            ?></span>
+                            <span class="event-summary__day"><?php echo $eventDate->format('d');; ?></span>
                         </a>
                         <div class="event-summary__content">
                             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
