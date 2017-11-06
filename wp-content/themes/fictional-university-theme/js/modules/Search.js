aba50e3043b7664f0b2de571d272f4eeb133a96c
Rafa Huelin <it@engisa.com>
2017-11-06 07:40:42 +0000

    Make url flexible

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -47,7 +47,7 @@ class Search {
     };
 
     getResults() {
-        $.getJSON('http://localhost:3000/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+        $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
             this.resultsDiv.html(`
                 <h2 class="search-overlay__section-title">General Information</h2>
                 ${posts.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search</p>'}
