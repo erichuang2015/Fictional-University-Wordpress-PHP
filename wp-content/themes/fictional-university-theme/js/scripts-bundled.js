3ef0172dbb0ff2c804eb62726d45723256294002
Rafa Huelin <it@engisa.com>
2017-11-06 09:19:07 +0000

    Send two requests synchronously

--- wp-content/themes/fictional-university-theme/js/scripts-bundled.js
+++ wp-content/themes/fictional-university-theme/js/scripts-bundled.js
@@ -10615,10 +10615,13 @@ var Search = function () {
             var _this = this;
 
             _jquery2.default.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), function (posts) {
-                _this.resultsDiv.html("\n                <h2 class=\"search-overlay__section-title\">General Information</h2>\n                " + (posts.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search</p>') + "\n                    " + posts.map(function (item) {
-                    return "<li><a href=\"item.link\">" + item.title.rendered + "</a></li>";
-                }).join('') + "\n                " + (posts.length ? '</ul>' : '') + "\n            ");
-                _this.isSpinnerVisible = false;
+                _jquery2.default.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + _this.searchField.val(), function (pages) {
+                    var combinedResults = posts.concat(pages);
+                    _this.resultsDiv.html("\n                <h2 class=\"search-overlay__section-title\">General Information</h2>\n                " + (combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search</p>') + "\n                    " + combinedResults.map(function (item) {
+                        return "<li><a href=\"item.link\">" + item.title.rendered + "</a></li>";
+                    }).join('') + "\n                " + (combinedResults.length ? '</ul>' : '') + "\n            ");
+                    _this.isSpinnerVisible = false;
+                });
             });
         }
     }, {
