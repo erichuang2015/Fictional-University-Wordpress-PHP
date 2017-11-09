9379aa50bd5c33710f2dc3bfe58b6167ce72ccfa
Rafa Huelin <it@engisa.com>
2017-11-09 13:15:22 +0000

    Custom layout and JSON based on post type

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -40,7 +40,8 @@ while(have_posts()) {
                     <a class="professor-card" href="<?php the_permalink(); ?>">
                         <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>">
                         <span class="professor-card__name"><?php the_title(); ?></span>
-                    </a></li>
+                    </a>
+                </li>
 			<?php }
 			echo '</ul>';
 		}
