45abd08bc6ccae9ca87b5b5c7ecd025c31325aba
Rafa Huelin <it@engisa.com>
2017-11-12 13:28:30 +0000

    My notes front end, nonce setup and delete note

--- wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -12,7 +12,21 @@ class MyNotes {
 
     // Methods will go here
     deleteNote() {
-        alert("you clicked delete");
+        $.ajax({
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+            },
+            url: universityData.root_url + '/wp-json/wp/v2/note/85' ,
+            type: 'DELETE',
+            success: (response) => {
+                console.log("Congrats");
+                console.log(response);
+            },
+            error: (response) => {
+                console.log("Sorry");
+                console.log(response);
+            }
+        });
     }
 }
 
--- wp-content/themes/fictional-university-theme/js/scripts-bundled.js
+++ wp-content/themes/fictional-university-theme/js/scripts-bundled.js
@@ -10573,7 +10573,21 @@ var MyNotes = function () {
     }, {
         key: "deleteNote",
         value: function deleteNote() {
-            alert("you clicked delete");
+            _jquery2.default.ajax({
+                beforeSend: function beforeSend(xhr) {
+                    xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+                },
+                url: universityData.root_url + '/wp-json/wp/v2/note/85',
+                type: 'DELETE',
+                success: function success(response) {
+                    console.log("Congrats");
+                    console.log(response);
+                },
+                error: function error(response) {
+                    console.log("Sorry");
+                    console.log(response);
+                }
+            });
         }
     }]);
 
