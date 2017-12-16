f8314ff4e067727b18f5f53b5a0a4dab2cbfbd62
Rafa Huelin <it@engisa.com>
2017-12-16 18:59:43 +0000

    Make note fields extra secure

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -149,6 +149,10 @@ function ourLoginTitle() {
 add_filter('wp_insert_post_data', 'makeNotePrivate');
 
 function makeNotePrivate ($data) {
+    if ($data['post_type'] == 'note') {
+        $data['post_content'] = sanitize_textarea_field($data['post_content']);
+	    $data['title'] = sanitize_text_field($data['title']);
+    }
     if($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
         $data['post_status'] = "private";
     }
