3cd44ea339900a562f83a23f22f4184b637cd352
Rafa Huelin <it@engisa.com>
2017-12-16 19:52:36 +0000

    Set maximum number of notes per user

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -146,12 +146,15 @@ function ourLoginTitle() {
 
 
 // Force note posts to be private
-add_filter('wp_insert_post_data', 'makeNotePrivate');
+add_filter('wp_insert_post_data', 'makeNotePrivate', 10, 2);
 
-function makeNotePrivate ($data) {
+function makeNotePrivate ($data, $postarr) {
     if ($data['post_type'] == 'note') {
+        if(count_user_posts(get_current_user_id(), 'note') > 5 AND !$postarr['ID']) {
+            die("You have reached the maximum number of notes");
+        }
         $data['post_content'] = sanitize_textarea_field($data['post_content']);
-	    $data['title'] = sanitize_text_field($data['title']);
+        $data['post_title'] = sanitize_text_field($data['post_title']);
     }
     if($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
         $data['post_status'] = "private";
